6a04832b11290b762d515e04c8575c199a4f35cf
Samrat-14 <samratdewdrops0@gmail.com>
2023-03-04 19:24:21 +0530

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,98 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import demo from "./demo.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import python from "./tech/python.png";
+import c from "./tech/c.png";
+import cPlusPlus from "./tech/cPlusPlus.png";
+import html from "./tech/html.png";
+import css from "./tech/css.png";
+import javascript from "./tech/javascript.png";
+import typescript from "./tech/typescript.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import materialUI from "./tech/materialUI.png";
+import tailwind from "./tech/tailwind.png";
+import bootstrap from "./tech/bootstrap.png";
+import django from "./tech/django.png";
+import nodejs from "./tech/nodejs.png";
+import mongodb from "./tech/mongodb.png";
+import mysql from "./tech/mysql.png";
+import postgres from "./tech/postgres.png";
+import threejs from "./tech/threejs.svg";
+import vscode from "./tech/vscode.png";
+import sublimetext from "./tech/sublimetext.png";
+import heroku from "./tech/heroku.png";
+import git from "./tech/git.png";
+import github2 from "./tech/github.png";
+import figma from "./tech/figma.png";
+
+// import starbucks from "./company/starbucks.png";
+import cantileverLabs from "./company/cantileverLabs.png";
+import gfg from "./company/gfg.jpg";
+import jgec from "./company/jgec.png";
+
+import tetris_game from "./projects/tetris_game.png";
+import personal_portfolio from "./projects/personal_portfolio.png";
+import wordle from "./projects/wordle.png";
+import sorting_visualizer from "./projects/sorting_visualizer_02.png";
+import travel_advisor from "./projects/travel_advisor.png";
+import share_me from "./projects/share_me.png";
+import studybud from "./projects/studybud.png";
+import expense_tracker from "./projects/expense_tracker.png";
+import bullseye_game from "./projects/bullseye_game.png";
+
+export {
+    logo,
+    backend,
+    creator,
+    mobile,
+    web,
+    demo,
+    github,
+    menu,
+    close,
+    css,
+    python,
+    c,
+    cPlusPlus,
+    django,
+    bootstrap,
+    mysql,
+    postgres,
+    vscode,
+    sublimetext,
+    heroku,
+    materialUI,
+    figma,
+    git,
+    github2,
+    html,
+    javascript,
+    mongodb,
+    nodejs,
+    reactjs,
+    redux,
+    tailwind,
+    typescript,
+    threejs,
+    jgec,
+    gfg,
+    // starbucks,
+    cantileverLabs,
+    bullseye_game,
+    expense_tracker,
+    studybud,
+    share_me,
+    travel_advisor,
+    sorting_visualizer,
+    wordle,
+    personal_portfolio,
+    tetris_game,
+};
